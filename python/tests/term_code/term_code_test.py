5753ec5c4068b887dd8940c0d4173509ef52f855
JJDLTorre <JJDLTorre@gmail.com>
2020-02-25 15:22:12 -0800

    Added PyTest to show doctest too - Added pytest.ini. - Updated the settings to allow PyTest to run doc tests.

--- python/tests/term_code/term_code_test.py
+++ python/tests/term_code/term_code_test.py
@@ -6,4 +6,4 @@ class TestTermCode(unittest.TestCase):
 
     def test_Winter_term_code(self):
         result = term_code.convert_from_term_code(2202)
-        self.assertEquals("Winter 2020", result)
+        self.assertEqual("Winter 2020", result)
